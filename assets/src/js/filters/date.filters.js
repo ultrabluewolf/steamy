9d3abb604a952d0bb207c06b962965d04819917b
B. Luckey <ultrabluewolf-fb2@yahoo.com>
2015-08-01 19:33:51 -0400

    Fix displayed playtime units

--- assets/src/js/filters/date.filters.js
+++ assets/src/js/filters/date.filters.js
@@ -9,4 +9,22 @@ app.filter('relativeTime', function (){
 
 });
 
+app.filter('minToHrs', function (){
+  
+  return function (minutes) {
+    var hrs = Math.floor(minutes / 60);
+    var mins = (minutes % 60);
+
+    var str = '';
+    if (hrs) {
+      str += hrs + 'h ';
+    }
+    if (mins) {
+      str += mins + 'm';
+    }
+    return str;
+  };
+
+});
+
 })();
